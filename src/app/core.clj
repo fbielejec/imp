2e08d06a692f33dd82220ef428096b2f4a616ac1
filip <fbielejec@gmail.com>
2016-03-14 14:26:25 +0100

    sort by key

--- src/app/core.clj
+++ src/app/core.clj
@@ -15,7 +15,7 @@
 
 (def coordinateName "location")
 
-(def nSlices 1000)
+(def nSlices 10)
 
 ;;;;;;;;;;;;;;;;;;;;;
 ;;---HERE WE GO!---;;
@@ -31,60 +31,60 @@
 
 (defn getRootCoords [tree]
   "Returns root coordinate attribute values"
-  (let  [rootNode (. tree getRootNode ) ] 
-    
+  (let  [rootNode (. tree getRootNode ) ]
+
     ( . rootNode getAttribute coordinateName )
-    
+
     );END:let
   );END: getRootCoords
 
 
-(defn getDistanceToRoot 
+(defn getDistanceToRoot
   "Returns great-cricle-distance distance between two coordinates"
   [nodeCoord rootCoord]
   (let [lat1 ( get  nodeCoord 1 ) long1 ( get  nodeCoord 0 ) lat2 ( get  rootCoord 1 ) long2 ( get  rootCoord 0 )]
-    
+
     (utils/great-circle-distance  lat1 long1 lat2 long2 )
-    
-    
+
+
     );END:let
   );END:getDistanceToRoot
 
 
 ; :node001 {:startX 0.1 :startY 0.3 :endX 0.5 :endY 0.4 :length 0.71 }
-(defn analyzeTree 
-  "Represents tree as a map of branches (node-parent pairs): 
+(defn analyzeTree
+  "Represents tree as a map of branches (node-parent pairs):
    :node001 {:startX 0.1 :startY 0.3 :endX 0.5 :endY 0.4 :length 0.71 }
    key is a node which represents the branch, value is a map of attributes we need later"
   [tree]
-  
+
   (into {}
-        
+
         (let [nodes (into #{}  (. tree getNodes )) rootCoord (getRootCoords tree) ]
-          
+
           (map  (fn [node]
                   (if  ( not (. tree isRoot node))
                     (let [parentNode (. tree getParent node)]
                       (let [nodeCoord ( . node getAttribute coordinateName ) parentCoord ( . parentNode getAttribute coordinateName ) ]
-                        
+
                         (hash-map node {
                                         :startX ( get  parentCoord 0 ) ; parent long
                                         :startY ( get  parentCoord 1 ) ;parent lat
                                         :endX  ( get  nodeCoord 0 ); long
                                         :endY  ( get  nodeCoord 1 ) ; lat
-                                        :startHeight (. tree getHeight node) 
+                                        :startHeight (. tree getHeight node)
                                         :endHeight (. tree getHeight parentNode)
                                         :length  (- (. tree getHeight parentNode)  (. tree getHeight node) )
                                         :distanceToRoot (getDistanceToRoot nodeCoord rootCoord)
                                         })
-                        
+
                         );END: let
                       );END: let
                     );END: if
                   );END: fn
                 nodes
                 );END: map
-          
+
           );END:let
         );END: into
   );END: analyzeTree
@@ -104,24 +104,24 @@
 (defn createSliceHeights [branchesMap]
   "Returns a sequence of length nSlices"
   (let [ minim (getMinStartTime branchesMap) maxim (getMaxStartTime branchesMap) by (/ ( - maxim minim)  nSlices ) ]
-    
-    (range minim maxim by) 
-    
+
+    (range minim maxim by)
+
     );END:let
   );createSliceHeights
 
 
-(defn filterBySlice 
+(defn filterBySlice
   "Return collection of branches which are intersected by sliceHeight"
   [branchesMap sliceHeight]
   (filter (fn [branch]
-            
-            (<=  (:startHeight ( val branch) )  sliceHeight (:endHeight ( val branch) ) ) 
-            
+
+            (<=  (:startHeight ( val branch) )  sliceHeight (:endHeight ( val branch) ) )
+
             );END:fn
           branchesMap
           );END: filter
-  );END:filterBySlice 
+  );END:filterBySlice
 
 
 ;furthest. is that a word?
@@ -130,70 +130,71 @@
   [branchesSubset]
   (let [ maxDist (apply max (map :distanceToRoot (vals branchesSubset)  ) )]
     (filter (fn [branch]
-              
-              (==  (:distanceToRoot ( val branch) ) maxDist) 
-              
+
+              (==  (:distanceToRoot ( val branch) ) maxDist)
+
               );END:fn
             branchesSubset
             );END: filter
-    );END:let 
+    );END:let
   );END: getFurthestFromRoot
 
 
-(defn getDistances 
+(defn getDistances
   "For every slice calculate spatial stats"
   [branchesMap tree]
-  
+
   (let [sliceHeights (createSliceHeights branchesMap)  ]
-    
+
     (reduce
       (fn [slicesMap sliceHeight ]
-        
+
         ;    get the branches  intersected by this slice
         (let [ branchesSubset ( filterBySlice branchesMap sliceHeight) ]
-          
-          ;  get the furthest one from root 
+
+          ;  get the furthest one from root
           (let [ furthestBranch (getFurthestFromRoot branchesSubset) ]
-            
+
             (let [dist (map :distanceToRoot ( vals furthestBranch) ) length (map :length ( vals furthestBranch) )  ]
-              
+
               (assoc slicesMap sliceHeight {
                                             :wavefrontDistance (/ (nth dist 0) (nth length 0) )
                                             } );END: assoc
-              
+
               );END:let
-            
+
             );END:let
           );END:let
-        
+
         );END:fn
       { } ;initial
       sliceHeights ;coll
       );END:reduce
-    
+
     );END:let
   );END: getDistances
 
 
-(defn treesLoop 
+(defn treesLoop
   "Iterate over trees distribution calculating spatial stats"
   []
 ;    (while (. treeImporter hasTree)
   (let [currentTree (. treeImporter importNextTree ) ]
-    
+
     (let [branchesMap (analyzeTree currentTree) ]
       (println
-        
-        
+
+
         (utils/toJSON
-          
+
+(into (sorted-map)
           ( getDistances branchesMap currentTree )
-          
-          )
-        
+);END: sorted-map
+          );END:toJSON
+
         );END: println
       );END:let
-    
+
     );END:let
 ;      );END: while
   );END: treesLoop
@@ -203,14 +204,14 @@
   "Entry point"
   [& args]
   (do
-    
+
     (time
       ( treesLoop )
       )
-    
+
     ( println "Done!" )
-    
+
     ( System/exit 0 )
-    
+
     );END;do
   );END: main
