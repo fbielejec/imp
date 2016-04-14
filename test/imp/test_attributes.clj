53c418c262fb72c43753638e9c5d81d15373830d
filip <fbielejec@gmail.com>
2016-04-14 15:30:58 +0200

    feeling thready

--- test/imp/test_attributes.clj
+++ test/imp/test_attributes.clj
@@ -8,26 +8,37 @@
   (:require [clj-json.core :as json] )
   (:require [imp.routes.handler :as h])
   (:require [imp.data.attributes :as a])
-  (:use clojure.data)
   )
 
 
-
 (deftest test-attributes
-  (testing "test location attributes parsing"
-           
-           (let [file  (slurp "/home/filip/Dropbox/ClojureProjects/imp-rest/resources/WNV_small.trees") ]
-             ;; mock a PUT request
-             (h/app 
-               (-> (mock/request
-                     :put
-                     "/trees"
-                     (json/generate-string { :input file}))
-                 (mock/content-type "application/json"))))  
-           
-           ;; parse attributes 
-           (is
-             (->
-               (a/parse-attributes)
-               (= #{"rate" "location"})))))
+  (let [expected-result (set ["rate" "location"])]
+    
+    ;; --- REGULAR TESTS---;;
+    
+    (testing "test location attributes parsing"
+             (let [file  (slurp "/home/filip/Dropbox/ClojureProjects/imp-rest/resources/WNV_small.trees") ]
+               ;; mock a PUT request
+               (h/app 
+                 (-> (mock/request
+                       :put
+                       "/trees"
+                       (json/generate-string { :input file}))
+                   (mock/content-type "application/json"))))  
+             
+             ;; parse attributes 
+             (let [result (a/parse-attributes)]
+               (->
+                 (set result)
+                 (= expected-result)
+                 (is))))
+    
+    ;; --- REST TESTS---;;
+    
+    (testing "test location attributes GET request"
+             (let [response (h/app (mock/request :get "/attributes"))]
+               (->
+                 (set (read-string (:body response)))
+                 (= expected-result)
+                 (is))))))
 
